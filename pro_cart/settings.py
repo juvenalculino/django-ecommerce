4c02e744c60c5800fa0fcaac4fcaa8e0ec3356dd
juvenalculino <b831381@gmail.com>
2022-01-08 16:58:55 -0300

    Adicionando funcionalidades do carrinho.

--- pro_cart/settings.py
+++ pro_cart/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'categoria',
     'contas',
     'loja',
+    'carrinho',
 ]
 
 MIDDLEWARE = [
--- pro_cart/urls.py
+++ pro_cart/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('loja/', include('loja.urls')) ,
+    path('carrinho/', include('carrinho.urls')) ,
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 

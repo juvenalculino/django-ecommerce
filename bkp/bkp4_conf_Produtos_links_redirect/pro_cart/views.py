3472c50376a0a2b0d395d6198fb31054fbafc2c2
Juvenal Culino <71508192+juvenalculino@users.noreply.github.com>
2022-01-08 16:29:09 -0300

    Delete bkp directory

--- bkp/bkp4_conf_Produtos_links_redirect/pro_cart/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from loja.models import Produto
-
-def home(request):
-    produtos = Produto.objects.all().filter(e_disponivel=True)
-    context = {
-        'produtos': produtos,
-    }
-    return render(request, 'home.html', context)

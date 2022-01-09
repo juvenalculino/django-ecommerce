d72a3dfe85fbc7804c2a79219552b3279946cfdb
juvenalculino <b831381@gmail.com>
2022-01-09 10:13:30 -0300

    salvando configuraçoes

--- loja/views.py
+++ loja/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from .models import Produto
 from categoria.models import Categoria
+from carrinho.models import CarrinhoItem
+from carrinho.views import _carrinho_id
 # Create your views here.
 
 def loja(request, categoria_slug=None):
@@ -25,6 +28,10 @@ def loja(request, categoria_slug=None):
 def detalhe_produto(request, categoria_slug, produto_slug):
     try:
         unico_produto = Produto.objects.get(categoria__slug=categoria_slug, slug=produto_slug)
+        #4
+        no_carro = CarrinhoItem.objects.filter(carrinho__carro_id=_carrinho_id(request), produto=unico_produto).exists()
+        return HttpResponse(no_carro)
+        exit()
     except Exception as e:
         raise e
     context = {

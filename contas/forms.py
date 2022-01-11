5dad149477b5942361fdab3022b04fa6ce971781
juvenalculino <b831381@gmail.com>
2022-01-11 19:00:36 -0300

    Registration  Implementing Model Forms and Editing __init__ Method

--- contas/forms.py
+++ contas/forms.py
@@ -1,11 +1,30 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, CharField
+from django.forms.widgets import PasswordInput
 
 from .models import Contas
 
 class ResgitrationForm(ModelForm):
+    password = CharField(widget=PasswordInput(attrs={
+        'placeholder': 'Digite a Senha',
+        'class': 'form-control',
+    }))
+    confirm_password = CharField(widget=PasswordInput(attrs={
+        'placeholder': 'Confirmar Senha',
+        'class': 'form-control',
+    }))
     class Meta:
         model = Contas
         fields = ['nome', 'sobrenome', 'telefone', 'email', 'password']
+    
+    def __init__(self, *args, **kwargs):
+        super(ResgitrationForm, self).__init__(*args, **kwargs)
+        self.fields['nome'].widget.attrs['placeholder'] = 'Seu Nome'
+        self.fields['sobrenome'].widget.attrs['placeholder'] = 'Seu Sobrenome'
+        self.fields['email'].widget.attrs['placeholder'] = 'Seu Email'
+        self.fields['telefone'].widget.attrs['placeholder'] = 'Seu Telefone'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
 
 '''
     nome

a8ad41713d264a9cb255f93f657f7b6fe80d86d6
juvenalculino <b831381@gmail.com>
2022-01-11 19:24:29 -0300

    Funçao registrar esta enviando os dados do registro para o db

--- contas/views.py
+++ contas/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from .forms import ResgitrationForm
+from .models import Contas
 
 
 # Create your views here.
 def register(request):
-    form = ResgitrationForm
+    if request.method == 'POST':
+        form = ResgitrationForm(request.POST)
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            sobrenome = form.cleaned_data['sobrenome']
+            email = form.cleaned_data['email']
+            telefone = form.cleaned_data['telefone']
+            username = email.split('@')[0]
+            password = form.cleaned_data['password']
+            # Vamos pasar aqui os dados que tem na nossa função do smodels def create_user(self, nome, sobrenome, username, email, password=None):
+            user = Contas.objects.create_user(nome=nome, sobrenome=sobrenome, email=email, username=username, password=password)
+            user.telefone = telefone
+            user.save()
+    else:
+        form = ResgitrationForm()
     context = {
         'form': form,
     }

60d92beeac42740bd8015c9780dd45fa39aacf93
juvenalculino <b831381@gmail.com>
2022-01-11 21:50:57 -0300

    Criando a funçao login | logout | messages

--- contas/views.py
+++ contas/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import ResgitrationForm
 from .models import Contas
-from django.contrib import messages
-
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def register(request):
@@ -30,7 +30,23 @@ def register(request):
     
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            #messages.success(request, 'Agora você está logado!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Erro ao fazer login')
+            return redirect('login')
     return render(request, 'contas/login.html')
 
+@login_required(login_url='login')
 def logout(request):
-    return render(request, 'contas/logout.html')
+    auth.logout(request)
+    messages.success(request, 'Você saiu do sistema!')
+    return redirect('login')
